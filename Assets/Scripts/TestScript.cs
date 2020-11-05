9fcb60c87592ed5357a43c57db2e21d78e6de3c2
JunHong30 <neojunhong2003@gmail.com>
2020-11-05 16:49:11 +0800

    Initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Start Function";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
